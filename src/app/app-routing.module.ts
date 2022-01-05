72f5472c1ca6e1160cdf089b1c36d802406ae4e9
toni <amvaratoni@gmail.com>
2022-01-05 15:47:23 +0000

    any url that is not include in app.routing-module.ts will now redirect to https://cometa.rocks/home, added nginx restart in prepare.sh. In case there are changes in nginx default.conf, deploy will now automatically apply it

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,31 +8,32 @@ const routes: Routes = [
   { path: 'home', component: CometaHomeComponent },
   { path: 'logotips', component: CometaLogotipsComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: '**', redirectTo: '' },
 
-  //redirects cometa.rocks/cometa-introduction to https://drive.google.com/file/d/1Y4lQ5aSC5Dbfn8q5Q4DLy0eEfsGWAVkm/view?usp=sharing
+  //redirects cometa.rocks/introduction to https://drive.google.com/file/d/1Y4lQ5aSC5Dbfn8q5Q4DLy0eEfsGWAVkm/view?usp=sharing
   {
     path: 'introduction',
     loadChildren: () => new Promise(() => { if (window.location.href.match(/introduction/)) window.location.href = 'https://drive.google.com/file/d/1Y4lQ5aSC5Dbfn8q5Q4DLy0eEfsGWAVkm/view?usp=sharing'; })
   },
 
-  //redirects cometa.rocks/cometa-getting-started to https://drive.google.com/file/d/1jWA5QeMW1MJ0rmrP60oC5YzpCSRcZa9w/view?usp=sharing
+  //redirects cometa.rocks/getting-started to https://drive.google.com/file/d/1jWA5QeMW1MJ0rmrP60oC5YzpCSRcZa9w/view?usp=sharing
   {
     path: 'getting-started',
     loadChildren: () => new Promise(() => { if (window.location.href.match(/getting-started/)) window.location.href = 'https://drive.google.com/file/d/1jWA5QeMW1MJ0rmrP60oC5YzpCSRcZa9w/view?usp=sharing'; })
   },
 
-  //redirects cometa.rocks/cometa-user-scenarios to https://drive.google.com/file/d/1jlxj-776_oTUyRHrxtFgZJUXmX3yInxn/view?usp=sharing
+  //redirects cometa.rocks/user-scenarios to https://drive.google.com/file/d/1jlxj-776_oTUyRHrxtFgZJUXmX3yInxn/view?usp=sharing
   {
     path: 'user-scenarios',
     loadChildren: () => new Promise(() => { if (window.location.href.match(/user-scenarios/)) window.location.href = 'https://drive.google.com/file/d/1jlxj-776_oTUyRHrxtFgZJUXmX3yInxn/view?usp=sharing'; })
   },
 
-  //redirects cometa.rocks/cometa-early-access-form to https://drive.google.com/file/d/1jlxj-776_oTUyRHrxtFgZJUXmX3yInxn/view?usp=sharing
+  //redirects cometa.rocks/early-access-form to https://drive.google.com/file/d/1jlxj-776_oTUyRHrxtFgZJUXmX3yInxn/view?usp=sharing
   {
     path: 'early-access-form',
     loadChildren: () => new Promise(() => { if (window.location.href.match(/early-access-form/)) window.location.href = 'https://ocagzojn33k.typeform.com/to/G2VgVbbB'; })
-  }
+  },
+
+  { path: '**', redirectTo: '/home' },
 ];
 
 @NgModule({
