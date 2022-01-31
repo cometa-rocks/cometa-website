8a9f4ac228a78a15ef88350d68b2992b56bec238
Ralf <ralf.roeber@amvara.de>
2022-01-31 08:25:40 +0100

    Adding CE, Cloud and Enterprise Descriptions

--- src/app/components/cometa-pricing/cometa-pricing.component.ts
+++ src/app/components/cometa-pricing/cometa-pricing.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+/* ViewEncapsulation makes css rules from this component visible in Angular - https://stackoverflow.com/questions/45940965/angular-material-customize-tab */
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { SwitcherService } from '../../cometa-services/shared/switcher.service';
 @Component({
   selector: 'app-cometa-pricing',
   templateUrl: './cometa-pricing.component.html',
-  styleUrls: ['./cometa-pricing.component.scss']
+  styleUrls: ['./cometa-pricing.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class CometaPricingComponent implements OnInit {
 
